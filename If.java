b567348c3f7a84340a94b4710484ecf2c480614e
dharsan05 <dharsanrajvir@gmail.com>
2019-03-14 23:12:16 +0530

    array

---
+++ If.java
@@ -0,0 +1,14 @@
+
+public class If {
+public static void main(String[] args) {
+	int i =25;
+	if (i==26) 
+		System.out.println("equal");
+
+	else
+	 
+		System.out.println("not equal");
+	 
+}
+}
+
---
+++ org/array/Array.java
@@ -0,0 +1,18 @@
+package org.array;
+
+public class Array {
+	public static void main(String[] args) {
+		int [] a = new int [5];
+		a[0] = 20;
+		a[1] = 50;
+		a[2] = 70;
+		a[3] = 10;
+		a[4] = 30;
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+			
+		}
+	}
+
+}
